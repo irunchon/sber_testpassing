937548ddcd961cfef6338cb7c58526c50a7794ac
Irina <irunchon@Irinas-MacBook-Air.local>
2023-09-01 17:53:32 +0300

    ver 0 1 file, 1 process

--- main.go
+++ main.go
@@ -14,13 +14,66 @@ import (
 )
 
 func main() {
-	passingTest("http://147.78.65.149/start/", "http://147.78.65.149/passed")
+	result := passingTest("http://147.78.65.149/start/", "http://147.78.65.149/passed")
+	if result == nil {
+		fmt.Println("Test successfully passed")
+	} else {
+		log.Println(result)
+	}
 }
 
-func passingTest(startURLStr, finalURLStr string) {
+func passingTest(startURL, finalURL string) error {
+	client, err := newHTTPClient()
+	if err != nil {
+		return fmt.Errorf("failed to create HTTP client: %s ", err)
+	}
+
+	response, err := responseToHTTPGetRequest(startURL, client)
+	if err != nil {
+		return fmt.Errorf("failed to get response for start page: %s ", err)
+	}
+	defer response.Body.Close()
+
+	location, locationError := response.Location()
+	if err != nil {
+		return fmt.Errorf("Failed to get URL from string %s: %s ", finalURL, err)
+	}
+
+	for locationError == nil && response.StatusCode == 302 && location.String() != finalURL {
+		fmt.Printf("%v\n", location) // TODO: delete!!!!!!!!!!!!!!!!!!!!!!!
+		response, err = responseToHTTPGetRequest(location.String(), client)
+		if err != nil {
+			return fmt.Errorf("failed to get response for page with question: %s ", err)
+		}
+		if response.StatusCode != 200 {
+			return fmt.Errorf("Wrong status code of response: %d ", response.StatusCode)
+		}
+
+		data, dataError := formAnswersForSending(response.Body)
+		if dataError != nil {
+			return fmt.Errorf("failed to form data with answers: %s ", dataError)
+		}
+		response, err = postAnswer(location.String(), client, data)
+		if err != nil {
+			return fmt.Errorf("failed to post answers: %s ", err)
+		}
+		// For not to exceed 3 requests per second:
+		time.Sleep(time.Second)
+		location, locationError = response.Location()
+	}
+	if response.StatusCode != 302 {
+		return fmt.Errorf("wrong status code of response: %d ", response.StatusCode)
+	}
+	if locationError != nil {
+		return fmt.Errorf("failed to get location in response: %s ", err)
+	}
+	return nil
+}
+
+func newHTTPClient() (*http.Client, error) {
 	jar, err := cookiejar.New(nil)
 	if err != nil {
-		log.Fatalf("Failed to create cookie jar: %v\n", err)
+		return nil, fmt.Errorf("Failed to create cookie jar: %s ", err)
 	}
 	client := &http.Client{
 		CheckRedirect: func(req *http.Request, via []*http.Request) error {
@@ -28,133 +81,107 @@ func passingTest(startURLStr, finalURLStr string) {
 		},
 		Jar: jar,
 	}
-
-	getRequest, err := http.NewRequest("GET", startURLStr, nil)
-	if err != nil {
-		log.Fatalf("Failed to create GET request: %v\n", err)
-	}
-	response, err := client.Do(getRequest)
-	defer response.Body.Close()
-	if err != nil {
-		log.Fatalf("Failed to get response to GET request: %v\n", err)
-	}
-
-	//var location *url.URL
-	//var locationError error
-	location, locationError := response.Location()
-	finalURL, err := url.Parse(finalURLStr)
-	if err != nil {
-		log.Fatalf("Failed to get URL from string %s: %v\n", finalURLStr, err)
-	}
-	fmt.Printf("location: %v\nfinalURL: %v\nfinalURL == location? %v\n\n", location, finalURL, finalURL == location) // delete!!!!!!!!!!!!
-
-	for response.StatusCode == 302 {
-		getRequest, err = http.NewRequest("GET", fmt.Sprint(location), nil)
-		if err != nil {
-			log.Fatalf("Failed to create request to %v: %v\n", location, err)
-		}
-		response, err = client.Do(getRequest)
-		if err != nil {
-			log.Fatalf("Failed to perform Get request for quesion: %v\n", err)
-		}
-		if response.StatusCode != 200 {
-			log.Fatalf("Wrong status code of response: %v\n", response.StatusCode)
-		}
-
-		fmt.Printf("Response to GET:\nstatus: %v\nlocation: %v\n", response.StatusCode, location) // delete!!!!!!!!!!!!
-		response = formAndPostAnswer(response.Body, location.String(), client)
-		time.Sleep(time.Second)
-		location, locationError = response.Location()
-		fmt.Printf("Response to POST:\nstatus: %v\nlocation: %v\n\n", response.StatusCode, location) // delete!!!!!!!!!!!!
-		if locationError != nil {
-			log.Fatalf("Failed to get location in response: %v\n", err)
-		}
-		if location.String() == finalURLStr {
-			fmt.Println("Test successfully passed")
-			break
-		}
-	}
+	return client, nil
 }
 
-func formAndPostAnswer(body io.ReadCloser, questionPage string, client *http.Client) *http.Response {
-	keysAndAnswers := parsingHTMLPage(body)
+func responseToHTTPGetRequest(url string, client *http.Client) (*http.Response, error) {
+	getRequest, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create GET request: %s ", err)
+	}
+	response, err := client.Do(getRequest)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get response to GET request: %s ", err)
+	}
+	return response, nil
+}
+
+func formAnswersForSending(body io.ReadCloser) (url.Values, error) {
+	keysAndAnswers, err := parsingHTMLPage(body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse HTML page: %s ", err)
+	}
 	data := url.Values{}
 	for k, v := range keysAndAnswers {
 		data.Set(k, v)
 	}
+	return data, nil
+}
+
+func postAnswer(questionPage string, client *http.Client, data url.Values) (*http.Response, error) {
 	postRequest, err := http.NewRequest("POST", questionPage, strings.NewReader(data.Encode()))
 	if err != nil {
-		log.Fatalf("Failed to create POST request for question (%s): %v\n", questionPage, err)
+		return nil, fmt.Errorf("failed to create POST request for question (%s): %s ", questionPage, err)
 	}
 	postRequest.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
 	postResponse, err := client.Do(postRequest)
-	defer postResponse.Body.Close()
 	if err != nil {
-		log.Fatalf("Failed to receive response to POST for question (%s)): %v\n", questionPage, err)
+		return nil, fmt.Errorf("failed to receive response to POST for question (%s)): %s ", questionPage, err)
 	}
-	return postResponse
+	defer postResponse.Body.Close()
+	return postResponse, nil
 }
 
-func parsingHTMLPage(r io.Reader) map[string]string {
+func parsingHTMLPage(r io.Reader) (map[string]string, error) {
 	parsedNode, err := html.Parse(r)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	questionOptions := make(map[string][]string)
-	var f func(*html.Node)
-	f = func(n *html.Node) {
-		if n.Type == html.ElementNode && n.Data == "input" {
-			var keyToAdd string
-			for _, attr := range n.Attr {
-				if attr.Key == "type" {
-					continue
+	findValuesForQuestionOptions(parsedNode, questionOptions)
+
+	answersByKeys := make(map[string]string)
+	for k, v := range questionOptions {
+		answersByKeys[k] = formAnswers(v)
+	}
+	return answersByKeys, nil
+}
+
+func findValuesForQuestionOptions(n *html.Node, questionOptions map[string][]string) {
+	if n.Type == html.ElementNode && n.Data == "input" {
+		var keyToAdd string
+		for _, attr := range n.Attr {
+			if attr.Key == "type" {
+				continue
+			}
+			if attr.Key == "name" {
+				keyToAdd = attr.Val
+				if _, isFound := questionOptions[keyToAdd]; !isFound {
+					questionOptions[keyToAdd] = make([]string, 0)
 				}
-				if attr.Key == "name" {
-					keyToAdd = attr.Val
-					if _, isFound := questionOptions[keyToAdd]; !isFound {
-						questionOptions[keyToAdd] = make([]string, 0)
-					}
-				}
-				if attr.Key == "value" {
+			}
+			if attr.Key == "value" {
+				questionOptions[keyToAdd] = append(questionOptions[keyToAdd], attr.Val)
+			}
+		}
+	}
+	if n.Type == html.ElementNode && n.Data == "select" {
+		var keyToAdd string
+		for _, attr := range n.Attr {
+			keyToAdd = attr.Val
+			if _, found := questionOptions[keyToAdd]; !found {
+				questionOptions[keyToAdd] = make([]string, 0)
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			if c.Data != "option" {
+				continue
+			}
+			for _, attr := range c.Attr {
+				if attr.Val != "" {
 					questionOptions[keyToAdd] = append(questionOptions[keyToAdd], attr.Val)
 				}
 			}
 		}
-		if n.Type == html.ElementNode && (n.Data == "select") {
-			var keyToAdd string
-			for _, attr := range n.Attr {
-				keyToAdd = attr.Val
-				if _, found := questionOptions[keyToAdd]; !found {
-					questionOptions[keyToAdd] = make([]string, 0)
-				}
-			}
-			for c := n.FirstChild; c != nil; c = c.NextSibling {
-				if c.Data != "option" {
-					continue
-				}
-				for _, attr := range c.Attr {
-					if attr.Val != "" {
-						questionOptions[keyToAdd] = append(questionOptions[keyToAdd], attr.Val)
-					}
-				}
-			}
-		}
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			f(c)
-		}
 	}
-	f(parsedNode)
-
-	answersByKeys := make(map[string]string)
-	for k, v := range questionOptions {
-		answersByKeys[k] = formAnswer(v)
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		findValuesForQuestionOptions(c, questionOptions)
 	}
-	return answersByKeys
 }
 
-func formAnswer(options []string) string {
+func formAnswers(options []string) string {
 	if len(options) == 0 {
 		return "test"
 	}
