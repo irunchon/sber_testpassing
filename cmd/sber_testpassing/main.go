419b242aaed7c4c1658824387b724fcf9c9e146f
Irina <irunchon@Irinas-MacBook-Air.local>
2023-09-03 21:53:36 +0300

    RPS parameter -> .env

--- cmd/sber_testpassing/main.go
+++ cmd/sber_testpassing/main.go
@@ -12,10 +12,24 @@ import (
 func main() {
 	startURL := os.Getenv("START_PAGE")
 	finalURL := os.Getenv("FINAL_PAGE")
+
 	qtyOfThreads, err := strconv.Atoi(os.Getenv("QTY_OF_THREADS"))
 	if err != nil {
 		log.Fatalf("Failed to parse quantity of threads: %s\n", os.Getenv("QTY_OF_THREADS"))
 	}
-	limiter := time.Tick(333 * time.Millisecond)
+
+	maxRPS, err := strconv.Atoi(os.Getenv("MAX_SERVER_RPS"))
+	if err != nil {
+		log.Fatalf("Failed to parse RPS parameter: %s\n", os.Getenv("MAX_SERVER_RPS"))
+	}
+
+	limiter := time.Tick(getTimeLimit(maxRPS))
 	passing_webtest.Runner(qtyOfThreads, startURL, finalURL, limiter)
+}
+
+func getTimeLimit(rps int) time.Duration {
+	if rps <= 0 {
+		return time.Second
+	}
+	return time.Duration(1000/rps) * time.Millisecond
 }
